f0d7e87852c23ad942e14ec7e1ed5566a0595314
WingTillDie <mychat.01a2bc3def@gmail.com>
2023-07-31 13:35:42 +0800

    Create set-title.bash

---
+++ set-title.bash
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+function set-title-echo() {
+    echo -en "\033]0;$*\a"
+}
+
+function set-title-prompt() {
+    PROMPT_COMMAND="echo -en \"\033]0;$*\a\""
+}
+
+function set-title-PS1() {
+  if [[ -z "$ORIG" ]]; then
+    ORIG=$PS1
+  fi
+  TITLE="\[\e]2;$*\a\]"
+  PS1=${ORIG}${TITLE}
+}
+
+function set-title() {
+    set-title-echo "$*"
+}
